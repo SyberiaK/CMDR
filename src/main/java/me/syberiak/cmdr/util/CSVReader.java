ba206d02ff197d0e4395513c9ef6f8bcc0377f04
SyberiaK <syberiakey@gmail.com>
2022-08-30 13:48:27 +0500

    Deleted unused classes and methods

--- src/main/java/me/syberiak/cmdr/util/CSVReader.java
+++ src/main/java/me/syberiak/cmdr/util/CSVReader.java
@@ -1,37 +1,10 @@
 package me.syberiak.cmdr.util;
 
 import java.io.*;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.nio.file.Files;
-import java.nio.charset.StandardCharsets;
 import java.util.List;
 import java.util.ArrayList;
 
 public class CSVReader {
-
-    public static List<String[]> readDataFromCSV(String filename, String delimiter) throws IOException {
-        return readDataFromCSV(Paths.get(new File(filename).getAbsolutePath()), delimiter);
-    }
-
-    public static List<String[]> readDataFromCSV(Path filepath, String delimiter) throws IOException {
-        List<String[]> data = new ArrayList<>();
-
-        try (BufferedReader br = Files.newBufferedReader(filepath,
-                StandardCharsets.US_ASCII)) {
-
-            String line = br.readLine();
-
-            while (line != null) {
-                String[] attributes = line.split(delimiter);
-                data.add(attributes);
-
-                line = br.readLine();
-            }
-        }
-
-        return data;
-    }
 
     public static List<String[]> readDataFromCSV(InputStream inputStream, String delimiter) throws IOException {
         List<String[]> data = new ArrayList<>();
--- src/main/java/me/syberiak/cmdr/util/Pair.java
+++
@@ -1,3 +0,0 @@
-package me.syberiak.cmdr.util;
-
-public record Pair<K, V>(K element0, V element1) { }
