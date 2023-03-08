267cc57a52f226dcf9973b9087389079d48ed057
SyberiaK <syberiakey@gmail.com>
2023-03-08 13:22:14 +0500

    Fix CSVReader encoding (which fixes a typo in music-discs-data.csv)

--- src/main/java/me/syberiak/cmdr/util/CSVReader.java
+++ src/main/java/me/syberiak/cmdr/util/CSVReader.java
@@ -1,24 +1,20 @@
 package me.syberiak.cmdr.util;
 
 import java.io.*;
+import java.nio.charset.StandardCharsets;
 import java.util.List;
 import java.util.ArrayList;
 
 public class CSVReader {
 
     public static List<String[]> readDataFromCSV(InputStream inputStream, String delimiter) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(inputStream, StandardCharsets.UTF_8));
         List<String[]> data = new ArrayList<>();
 
-        try (BufferedReader br = new BufferedReader(new InputStreamReader(inputStream))) {
-
-            String line = br.readLine();
-
-            while (line != null) {
-                String[] attributes = line.split(delimiter);
-                data.add(attributes);
-
-                line = br.readLine();
-            }
+        String line;
+        while ((line = br.readLine()) != null) {
+            String[] attributes = line.split(delimiter);
+            data.add(attributes);
         }
 
         return data;
